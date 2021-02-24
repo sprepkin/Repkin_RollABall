d9c9035b78d33e6367d52208a79f4857a6dcd625
srepkin <srepkin@iu.edu>
2021-02-23 21:54:00 -0500

    Added variables and update function

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,28 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void OnMove(InputValue movementValue)
     {
+        Vector2 movementVector = movementValue.Get<Vector2>();
 
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement);
     }
 }
