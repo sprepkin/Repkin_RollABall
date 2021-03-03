f0fa988d8a402b373ab849e99953db408ab68d1d
srepkin <srepkin@iu.edu>
2021-03-02 20:47:02 -0500

    commented jump pad and collectible scripts

--- Rollaball/Assets/Scripts/MoveBackAndForth.cs
+++ Rollaball/Assets/Scripts/MoveBackAndForth.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveBackAndForth : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed = 1f; //creates speed for collectibles
 
     Vector3 beginPos;
     Vector3 endPos;
@@ -13,16 +13,19 @@ public class MoveBackAndForth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 moveTo = new Vector3(Random.Range(-7, 7), Random.Range(3, 7), Random.Range(-7, 7));
-        beginPos = transform.position;
-        endPos = beginPos + moveTo;
+        
+        Vector3 moveTo = new Vector3(Random.Range(-7, 7), Random.Range(3, 7), Random.Range(-7, 7)); //Creates a random spot for the collectible to move to
+        beginPos = transform.position; //Gets the beginning position of collectible
+        endPos = beginPos + moveTo; //Sets a value for the end position
     }
 
     // Update is called once per frame
     void Update()
     {
-        float progress = Mathf.PingPong(Time.time * speed, 1.2f);
+        //Progress value goes back and forth between 1 and 0 over time
+        float progress = Mathf.PingPong(Time.time * speed, 1f);
 
+        //Interpolates a line from the starting point to the random end point and moves the collectible between there
         transform.position = Vector3.Lerp(beginPos, endPos, progress);
     }
 }
--- Rollaball/Assets/Scripts/Rotator.cs
+++ Rollaball/Assets/Scripts/Rotator.cs
@@ -9,6 +9,6 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime); //rotate collectible over time
     }
 }
